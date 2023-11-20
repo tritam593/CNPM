b29e3004a42f34a812a83cef25d7860367c200f8
tam <meomeo050903@gmail.com>
2023-11-20 14:51:51 +0700

    test

--- test_golang/test/main.go
+++ test_golang/test/main.go
@@ -1,46 +1,105 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
-	"test/helper"
+	"strconv"
 )
 
-// func formHandler(w http.ResponseWriter, r *http.Request) {
-// 	if err := r.ParseForm(); err != nil {
-// 		fmt.Fprintf(w, "ParseForm() err: %v", err)
-// 		return
-// 	}
-// 	fmt.Fprintf(w, "POST request successful")
-// 	name := r.FormValue("name")
-// 	address := r.FormValue("address")
-// 	fmt.Fprintf(w, "Name = %s\n", name)
-// 	fmt.Fprintf(w, "Address = %s\n", address)
-	
-// }
+// the tag `json:"firstname"` just use to for define the key when encode to JSON format 
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
 
-// func helloHandler(w http.ResponseWriter, r *http.Request) {
-// 	if r.URL.Path != "/hello" {
-// 		http.Error(w, "404 not found", http.StatusNotFound)
-// 		return
-// 	}
-// 	if r.Method != "GET" {
-// 		http.Error(w, "method is not supported", http.StatusNotFound)
-// 		return
-// 	}
-// 	fmt.Fprintf(w, "hello!")
-// }
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	param := mux.Vars(r)
+	for index, item := range movies {
+
+		if item.ID  == param["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := mux.Vars(r)
+	for _, item := range movies {
+
+		if item.ID  == param["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID  == param["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = param["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
 
 func main() {
-	helper.Print_string("hello")
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", helper.FormHandler)
-	http.HandleFunc("/hello", helper.HelloHandler)
+	r := mux.NewRouter()
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	movies = append(movies, Movie{ID: "1", Isbn: "12345", Title: "Movie one",
+		Director: &Director{FirstName: "Tam", LastName: "Tri"}})
+
+	movies = append(movies, Movie{ID: "2", Isbn: "22345", Title: "Movie two",
+		Director: &Director{FirstName: "Tam", LastName: "Tri"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting at port 127.0.0.1:8000")
+	log.Fatal(http.ListenAndServe("127.0.0.1:8000", r))
+
 }
