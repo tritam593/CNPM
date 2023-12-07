1f9aa05ec0fdeedea3180a70e8fce6ce4b5c21f8
tam <meomeo050903@gmail.com>
2023-12-07 22:50:51 +0700

    test e-commerce

---
+++ test_golang/test/e-commerce-test/pkg/models/user.go
@@ -0,0 +1,89 @@
+package models
+
+import (
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID            string `gorm:"size:36;not null;uniqueIndex;primary_key"`
+	Addresses     []Address
+	FirstName     string `gorm:"size:100;not null"`
+	LastName      string `gorm:"size:100;not null"`
+	Email         string `gorm:"size:100;not null;uniqueIndex"`
+	Password      string `gorm:"size:255;not null"`
+	RememberToken string `gorm:"size:255;not null"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt
+}
+
+func (u *User) FindByEmail(db *gorm.DB, email string) (*User, error) {
+	var err error
+	var user User
+
+	err = db.Debug().Model(User{}).Where("LOWER(email) = ?", strings.ToLower(email)).
+		First(&user).
+		Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (u *User) FindByID(db *gorm.DB, userID string) (*User, error) {
+	var err error
+	var user User
+
+	err = db.Debug().Model(User{}).Where("id = ?", userID).
+		First(&user).
+		Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// CreateUser creates a new user in the database using the provided User parameter.
+//
+// Parameters:
+//   - db: A pointer to the Gorm database instance.
+//   - param: A pointer to the User struct containing user details.
+//
+// Returns:
+//   - *User: A pointer to the newly created User instance.
+//   - error: An error, if any, encountered during the database operation.
+//
+// Example:
+//   user := &User{
+//       ID:        1,
+//       FirstName: "John",
+//       LastName:  "Doe",
+//       Email:     "john.doe@example.com",
+//       Password:  "hashed_password",
+//   }
+//   createdUser, err := CreateUser(databaseInstance, user)
+//   if err != nil {
+//       log.Fatal(err)
+//   }
+//   fmt.Printf("User created with ID: %d\n", createdUser.ID)
+func (u *User) CreateUser(db *gorm.DB, param *User) (*User, error) {
+	user := &User{
+		ID:        param.ID,
+		FirstName: param.FirstName,
+		LastName:  param.LastName,
+		Email:     param.Email,
+		Password:  param.Password,
+	}
+
+	err := db.Debug().Create(&user).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
--- test_golang/test/go.mod
+++ test_golang/test/go.mod
@@ -3,7 +3,9 @@ module test
 go 1.21.4
 
 require (
+	github.com/google/uuid v1.4.0
 	github.com/gorilla/mux v1.8.1
+	github.com/shopspring/decimal v1.3.1
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.5
 )
