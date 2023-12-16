708a896a99b68e10e388435f0d314e1bb3432a59
Dang_Thang <dinhthanghp03@gmail.com>
2023-12-17 00:11:22 +0700

    show product in show page

---
+++ frontend/js/showProduct.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".pro-container");
+
+const jsonFile = "../products.json";
+fetch(jsonFile)
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		data.forEach((product) => {
+            const { ProductImages, Name, Price } = product;
+
+            // Tạo các div .pro cho mỗi hình ảnh trong mảng ProductImages
+            ProductImages.forEach((image) => {
+                container.innerHTML +=`
+                <div class="pro" onclick="window.location.href='sproduct.html'">
+                    <img src="${image.Path}" alt="Product Image">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>${Name}</h5>
+                        <div class="star">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                        <h4>${Price}</h4>
+                    </div>
+                    <a href="#" class="cart"><i class="fa-solid fa-cart-shopping"></i></a>
+                </div>
+                `;
+            });
+        });
+    });
