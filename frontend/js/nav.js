0fbc4b72b5339d8a16bbc93d6ca21757dc7f72f4
Dang_Thang <dinhthanghp03@gmail.com>
2023-12-28 09:03:00 +0700

    fix link page and nav

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -3,15 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    var isLoggedIn = false; 
-
+    var isLoggedIn = true; 
+    var isIndexPage = window.location.pathname.endsWith('index.html');
 
     if (isLoggedIn) {
         nav.innerHTML += '<li id="lg-user"><a href=""><i class="far fa-user"></i></a></li>';
         nav.innerHTML += '<li id="logout"><a href="">Logout</a></li>';
+
     } else {
-        nav.innerHTML += '<li id="sign-in"><a href="../sub-pages/sign-in.html">Sign in</a></li>';
-        nav.innerHTML += '<li id="sign-up"><a href="../sub-pages/sign-up.html">Sign up</a></li>'
+        if(isIndexPage){
+            nav.innerHTML += '<li id="sign-in"><a href="./sub-pages/sign-in.html">Sign in</a></li>';
+            nav.innerHTML += '<li id="sign-up"><a href="./sub-pages/sign-up.html">Sign up</a></li>';
+        }
+        else{
+            nav.innerHTML += '<li id="sign-in"><a href="../sub-pages/sign-in.html">Sign in</a></li>';
+            nav.innerHTML += '<li id="sign-up"><a href="../sub-pages/sign-up.html">Sign up</a></li>';
+        }
+        
     }
 });
 
