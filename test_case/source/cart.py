88ae9c0e80226bcba1ae29d7f7cb2ac737d68d36
trangphan10 <phantrang5656@gmail.com>
2024-01-02 12:51:39 +0700

    uptodate

--- test_case/source/cart.py
+++ test_case/source/cart.py
@@ -1,16 +1,17 @@
 import requests
-class Item: 
-    def __init__(self,img,name,price,qty):
-         self.img = img
-         self.name = name 
-         self.price = price
-         self.qty = qty
+
       
 class Cart: 
     def __init__(self,user_id,cart_item_id):
         self.user_id = user_id
         self.cart_item_id = cart_item_id
-        self.user_cart = []
+        self.id = ""
+        self.data = {
+                    "CartID": "7c39f4c4-06e0-4b91-972a-7eee52176169",
+                    "productID": "df64bd0d-809a-4321-93b9-8551552d17b4",
+                    "Qty": 10,
+                }
+
     def get_cart(self):
         r = requests.get(url=f"http://127.0.0.1:9000/carts/{self.user_id}")
         if r.status_code == 200: 
@@ -18,18 +19,19 @@ class Cart:
             return self.user_cart
         
         raise requests.HTTPError
-    def add_item(self,item):
-        r = requests.post(url=f"http://127.0.0.1:9000/carts")
+    def add_item(self):
+        r = requests.post(url=f"http://127.0.0.1:9000/carts",json=self.data)
+        all_cart = self.get_cart()
+        self.id = next((item["ID"] for item in all_cart if item["productID"]== "df64bd0d-809a-4321-93b9-8551552d17b4"), None)
         if r.status_code == 200:
-            self.user_cart.append(item)
-            return self.user_cart
+           return r.json()
         raise requests.HTTPError
         
     
-    def delete_item(self,item):
+    def delete_item(self):
         r = requests.delete(url=f"http://127.0.0.1:9000/carts/{self.cart_item_id}")
         if r.status_code == 200:
-            return self.user_cart.pop(item)
+           return r.json()
         raise requests.HTTPError
             
         
--- test_case/tests/test_cart.py
+++ test_case/tests/test_cart.py
@@ -4,26 +4,39 @@ class TestCart(unittest.TestCase):
     def setUp(self):
         # Khởi tạo một đối tượng User cho mỗi test case
         # self.user.login()
-        self.cart = cart.Cart('1ae0725f-af6f-49f1-abe7-110be47adc4e')
-        self.item = cart.Item("","Balo",'2000','2')
+        self.cart = cart.Cart('b77a02d5-9c62-4e15-9d20-3e3c05d287ac','7c39f4c4-06e0-4b91-972a-7eee52176169')
         print("====== set up ======")
-    
+    def tearDown(self) -> None:
+        if(self.cart.id != ""):
+            r = self.cart.delete_item()
+            print(r)
+            all_cart = self.cart.get_cart()
+            check_delete_cart = any(item["productID"] == self.cart.id for item in all_cart)
+            self.assertFalse(check_delete_cart)
+        
+        print("Tearing down test...")
     def test_get_cart(self):
         cart_temp = self.cart.get_cart() 
         print(cart_temp)
         self.assertTrue(len(cart_temp) > 1)
-    def test_add_item(self):
         
-         cart_temp = self.cart.get_cart() 
-         print(cart_temp)
-         self.cart.add_item(self.item)
-         cart_curr = self.cart.get_cart() 
-         self.assertTrue(len(cart_curr) > len(cart_temp))
+    def a_cart_is_empty(self):
+        cart_temp = self.cart.get_cart() 
+        self.assertTrue(len(cart_temp) == 1)
+        
+    def test_add_item(self):
+        print("====================test_add_cart==================")
+        self.cart.add_item()
+        all_cart = self.cart.get_cart()
+        check_add_cart = any(item["ProductID"] == "df64bd0d-809a-4321-93b9-8551552d17b4" for item in all_cart)
+        self.assertTrue(check_add_cart)
     def test_delete_item(self): 
         print("====================test_delete_cart==================")
+        self.cart.add_item()
+        print(self.cart.id)
         r = self.cart.delete_item()
         print(r)
-        all_product = self.cart.get_cart()
-        check_delete_product = any(item["ID"] == product_id for item in all_product)
-        self.assertFalse(check_delete_product)
+        all_cart = self.cart.get_cart()
+        check_delete_item = any(item["ID"] == self.cart.id for item in all_cart)
+        self.assertFalse(check_delete_item)
         
